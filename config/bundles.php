f619687d10fdbbc4836e65ac764b03d14f18e63e
NicoRachiu <rachiunicu@gmail.com>
2024-04-25 13:03:17 +0200

    Create a Database and Entities

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-];
+    ];
